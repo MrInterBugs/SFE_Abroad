b1e8ac6c28e16b17de9d644a7e7b4dcd2cf2e90a
Aedan Lawrence <aedan@mrinterbugs.uk>
2024-09-07 11:25:57 +0000

    Fixed csp settings

--- student-loan-repayment/app.js
+++ student-loan-repayment/app.js
@@ -12,8 +12,26 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser()); 
-app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "default-src": ["'self'"],
+      "script-src": [
+        "'self'",
+        "'unsafe-inline'",
+        "https://code.jquery.com",
+        "https://cdn.jsdelivr.net",
+      ],
+      "script-src-attr": ["'self'", "'unsafe-inline'"],
+      "style-src": ["'self'", "https://cdn.jsdelivr.net", "https://code.jquery.com"],
+      "img-src": ["'self'", "data:"],
+      "connect-src": ["'self'"],
+    },
+  })
+);
+
 
 // Views setup
 app.set('view engine', 'ejs');
