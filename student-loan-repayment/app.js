2bf05ea606ee7b30ab706be49deb5e089365df0e
Aedan Lawrence <aedan@mrinterbugs.uk>
2026-04-30 16:43:22 +0200

    Allow cloudflare scripts in csp

--- student-loan-repayment/app.js
+++ student-loan-repayment/app.js
@@ -54,7 +54,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       'default-src': ["'self'"],
-      'script-src': ["'self'", 'https://pagead2.googlesyndication.com', 'https://*.adtrafficquality.google', "https://consent.cookiebot.com/uc.js"],
+      'script-src': ["'self'", 'https://pagead2.googlesyndication.com', 'https://*.adtrafficquality.google', 'https://consent.cookiebot.com', 'https://static.cloudflareinsights.com'],
       'style-src': ["'self'"],
       'font-src': ["'self'"],
       'img-src': ["'self'", 'data:', 'https:'],
